0a6331de93029572f410805d722042128d2a343b
P1ZZ4 <30331389+P1ZZ4@users.noreply.github.com>
2021-08-31 20:33:36 +0900

    Create preprocessing.py

---
+++ RDP/preprocessing.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+plt.rcParams['figure.figsize'] = (15, 5)
+plt.rcParams['font.family'] = 'sans-serif'
+pd.set_option('display.width', 5000) 
+pd.set_option('display.max_columns', 60)
+
+df=pd.read_csv('data.csv')
+
+## 
