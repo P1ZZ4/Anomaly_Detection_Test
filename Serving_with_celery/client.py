709a425163ecdecd47af42a61bfbf97e10e4a188
P1ZZ4 <30331389+P1ZZ4@users.noreply.github.com>
2021-09-07 10:54:00 +0900

    Create client.py

---
+++ Serving_with_celery/client.py
@@ -0,0 +1,9 @@
+import requests
+
+#Example of sending test data
+def send_data_for_predict(data):
+    r = requests.get('http://127.0.0.1:8080', params={'data': data})
+    print("DATA: ", r)
+    print("hi")
+
+send_data_for_predict([0.4,0.5,0.7,0.1])
