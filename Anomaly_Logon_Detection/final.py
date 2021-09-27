89f00b31fb2fdbba6263a6387f904414f0f0c282
P1ZZ4 <30331389+P1ZZ4@users.noreply.github.com>
2021-09-27 16:53:37 +0900

    update feature(total_c)

--- Anomaly_Logon_Detection/final.py
+++ Anomaly_Logon_Detection/final.py
@@ -1,3 +1,100 @@
+# 업데이트 버전
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+#from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_validate
+import joblib
+%matplotlib inline
+
+df1 = pd.read_csv("user_good.csv")
+df2 = pd.read_csv("user_bad.csv")
+df = pd.concat([df1,df2])
+
+ 
+feature_cols = ['isnewuser','isnewip','isvpn','islan','percent','src_ip_c']
+X = df[feature_cols]
+y = df['tag']
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
+
+
+le = LabelEncoder()
+le.fit(y_train)
+print(le.classes_) #['N' 'Y']
+#labels = le.classes_
+targetname = ['Normal','Malicious']
+model = KNeighborsClassifier(n_neighbors=2)
+cv_results = cross_validate(model, X_train, y_train, return_estimator=True) #내부적으로 fit
+
+x_test = pd.DataFrame(x_test, columns=['islan','isnewip','isnewuser','isvpn','percent','src_ip_c'])
+
+best_index = np.argsort(cv_results['test_score'])[-1]
+
+best_model = cv_results['estimator'][best_index]
+
+joblib.dump(best_model, 'best_model3.pkl')
+
+
+
+# 테스트
+
+import joblib
+import os
+import pandas as pd
+
+MODEL_PATH = "best_model3.pkl"
+
+class SysModel:
+
+    def __init__(self):
+        self.model = self._load_model_from_path(MODEL_PATH)
+
+    @staticmethod
+    def _load_model_from_path(path):
+        model = joblib.load(path)
+        return model
+
+    def predict(self, data, return_option='Prob'):
+        #df = pd.DataFrame(data)
+        df = pd.DataFrame(x_test, columns=['islan','isnewip','isnewuser','isvpn','percent','src_ip_c'])
+        if return_option == 'Prob':
+            predictions = self.model.predict(df) #추후 추가
+        else:
+            predictions = self.model.predict(df)
+        return predictions
+      
+model = SysModel()
+x_test = [
+    ['1','1','1','1','0','0']
+] 
+
+targetname = ['Normal','Malicious']
+y_predict = model.predict(x_test)
+label = targetname[y_predict[0]]
+#model.predict(x_test)
+print(label)
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
@@ -86,3 +183,4 @@ label = targetname[y_predict[0]]
 #model.predict(x_test)
 print(label)
 
+"""
